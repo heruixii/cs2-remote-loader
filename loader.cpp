688fc96edf328dd54d7812b7a173603f3166a5b7
heruixii <2906680271@qq.com>
2026-07-15 03:29:40 +0800

    update URL to 469c6a4

--- loader.cpp
+++ loader.cpp
@@ -25,7 +25,7 @@
 // ============================================================
 
 // Payload 下载地址 — 从 GitHub 下载
-static const wchar_t* PAYLOAD_URL = L"https://raw.githubusercontent.com/heruixii/cs2-remote-loader/8290455/payload.dat";
+static const wchar_t* PAYLOAD_URL = L"https://raw.githubusercontent.com/heruixii/cs2-remote-loader/469c6a4/payload.dat";
 
 // 下载超时 (毫秒)
 static const DWORD DOWNLOAD_TIMEOUT_MS = 30000;
